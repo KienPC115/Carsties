5317ac15316e26724a3fe35426b1113b556cc129
KienPC115 <letrungkien2463@gmail.com>
2024-01-15 09:02:13 +0700

    End of section 5

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,7 @@
 using AuctionService;
 using AuctionService.Data;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -44,10 +45,22 @@ builder.Services.AddMassTransit(x => {
     });
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        // we need to give it an authority which in this case is our IdentityServer
+        // tell our resource server who the token was issued by
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false; // set to false bc our IdentityServer is runing on Http
+        options.TokenValidationParameters.ValidateAudience = false;
+        // setup the NameClaimType is corresponding wilt the username claim of token
+        // so if we get by User.Identity.Name -> this will be get the value of username in token
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ src/IdentityService/Services/CustomProfileService.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Claims;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService;
+
+public class CustomProfileService : IProfileService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public CustomProfileService(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    // We want to extend the token which IdentityServer return for us, 
+    //we add some Claims for the payload part of token
+    public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        var user = await _userManager.GetUserAsync(context.Subject); // context.Subject is represent the user Id
+        
+        // get the existing Claims, the only claim that we really added for our user was the fullname and we store that in the NameClaim
+        var existingClaims = await _userManager.GetClaimsAsync(user);
+
+        var claims = new List<Claim>
+        {
+            new Claim("username", user.UserName)
+        };
+
+        // we add the username, and the user's fullname in the token
+        context.IssuedClaims.AddRange(claims);
+        context.IssuedClaims.Add(existingClaims.FirstOrDefault(x => x.Type == JwtClaimTypes.Name));
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
